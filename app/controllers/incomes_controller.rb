cba625640e81d4d715201f3ad375d8bdc197328a
Jacob Green <jacob@nationbuilder.com>
2012-12-09 08:24:08 -0800

    Refactor

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -3,8 +3,22 @@ class IncomesController < ApplicationController
   end
 
   def create
-    Income.add_to_spreadsheet(Date.parse(params[:income][:date_string]) ,params[:income][:description], Money.parse(params[:income][:amount]))
-    Account.by_name("Checking").add(Money.parse(params[:income][:amount]))
+    Income.add_to_spreadsheet(date, description, amount)
+    Account.by_name("Checking").add(amount)
     redirect_to new_income_url
   end
+
+  private
+
+    def date
+      Date.parse(params[:income][:date_string])
+    end
+
+    def description
+      params[:income][:description]
+    end
+
+    def amount
+      Money.parse(params[:income][:amount])
+    end
 end
