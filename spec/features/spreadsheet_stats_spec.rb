bad5b969510a39621bdd722ae9852ac38a029a07
Jacob Green <jacob@nationbuilder.com>
2012-12-10 22:07:42 -0800

    Format spreadsheet category stats by period

--- spec/features/spreadsheet_stats_spec.rb
+++ spec/features/spreadsheet_stats_spec.rb
@@ -11,10 +11,15 @@ feature "Spreadsheet Stats" do
     Expense.report(Date.today, "Three", "Food", Money.new(1500), "Credit Card")
 
     visit "/"
-    within("#category-stats") do
+    within('#monthly') do
       page.should have_content "Food $25.00 $150.00 $125.00"
       page.should have_content "Rent $1,200.00 $1,200.00 $0.00"
       page.should have_content "Phone $0.00 $0.00 $0.00"
+    end
+
+    within('#yearly') do
+      page.should have_content "Medical $0.00"
+      page.should_not have_content "Medical $0.00 $0.00"
     end
   end
 
